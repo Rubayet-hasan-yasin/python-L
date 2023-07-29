30cccdd1601f1aeb8bd1c7bc807a7a261d4a10f6
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-07-29 08:31:48 +0600

    formated sring

---
+++ Part-9/string.py
@@ -0,0 +1,10 @@
+# string concatinate
+first_name = 'yasin'
+last_name = 'arafat'
+
+# print(first_name + " " + last_name)
+
+# formatted string f or F
+full_name = f"{first_name} {last_name} length of name {len(first_name + last_name)}"
+
+print(full_name)
