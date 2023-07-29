f1afae9a81e8e2ac4bbbc7d58d77eac3cf4ae66c
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-07-29 07:59:25 +0600

    slice in pyton

--- Part-7/string.py
+++ Part-7/string.py
@@ -10,6 +10,9 @@ name = 'rubayet hasan yasin'
 # length = len(name)
 # print(length)
 
-print(name[3])
-print(name[-3])
+# print(name[3])
+# print(name[-3])
 
+#slice string
+print(name[0:4])
+print(name[3:])
