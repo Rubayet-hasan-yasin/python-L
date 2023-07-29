bd65dc859b4d718a2a4b76f003ba3b1dbe0b4cb8
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-07-29 08:15:25 +0600

    escap sequence

---
+++ part-8/escape.py
@@ -0,0 +1,17 @@
+text1 = "This is 'python' tutorial"
+text2 = 'This is "python" tutorial'
+# escape sequence
+text3 = 'This is \'python\' tutorial'
+
+#if need to print backslash
+text4 = "This is \\ python tutorial"
+
+#for new line 
+text5 = "This is \npython tutorial"
+
+
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+print(text5)
